1d6d272e1edd652f7ba28f29dfbfa61888726bc5
lucKulke <lucaskulke98@gmail.com>
2024-11-08 22:21:07 +0100

    switched to postgres db

--- hub/src/db/db_init.py
+++ hub/src/db/db_init.py
@@ -1,19 +1,30 @@
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from .models import Base
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sqllite.db"
+DB_TYPE = os.getenv("DB_TYPE")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_ADDRESS = os.getenv("DB_ADDRESS")
+
+SQLALCHEMY_DATABASE_URL = f"{DB_TYPE}://{DB_USER}:{DB_PASSWORD}@{DB_ADDRESS}/{DB_NAME}"
+
 
 # Set up the database engine
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # Create session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Create the database tables
 Base.metadata.create_all(bind=engine)
+
 
 # Dependency to get the DB session
 def get_db():
